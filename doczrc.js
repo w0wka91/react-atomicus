cd485345b86ebc2e511af51b722afbbd3c126b9a
Waldemar Penner <Waldemar.Penner91@googlemail.com>
2018-11-21 22:00:53 +0100

    Add theme config to docz

--- doczrc.js
+++ doczrc.js
@@ -2,4 +2,15 @@ module.exports = {
   title: 'React Atoms',
   typescript: true,
   wrapper: 'src/DoczWrapper.tsx',
+  themeConfig: {
+    styles: {
+      playground: {
+        display: 'flex',
+        alignItems: 'center',
+        '*:not(:last-child)': {
+          marginRight: '1rem'
+        }
+      }
+    }
+  }
 }
