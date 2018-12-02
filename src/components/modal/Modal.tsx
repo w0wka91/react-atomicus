476970af1d8fef5eceb593d8b60bdc06deeee339
Waldemar Penner <Waldemar.Penner91@googlemail.com>
2018-12-02 15:55:23 +0100

    Add modal content and footer component

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useContext } from 'react'
-import { css } from 'emotion'
+import { css, cx } from 'emotion'
 import { shadows } from '../../utils/shadows'
 import { borders } from '../../utils/borders'
 import { colors } from '../../utils/colors'
@@ -14,11 +14,20 @@ const ModalContext = createContext({
   onClose: () => {},
 })
 
-function Modal({ open, onClose, children }: Props) {
+function Modal({
+  open,
+  onClose,
+  children,
+  className,
+  ...rest
+}: Props & React.HTMLProps<HTMLDivElement>) {
+  const escFunc = (ev: KeyboardEvent) => {
+    if (ev.keyCode === 27) onClose()
+  }
   useEffect(
     () => {
-      if (open) document.addEventListener('keydown', onClose)
-      return () => document.removeEventListener('keydown', onClose)
+      if (open) document.addEventListener('keydown', escFunc, false)
+      return () => document.removeEventListener('keydown', escFunc, false)
     },
     [open]
   )
@@ -44,21 +53,25 @@ function Modal({ open, onClose, children }: Props) {
       >
         <div
           onClick={e => e.stopPropagation()}
-          className={css`
-            background-color: #fff;
-            display: flex;
-            flex-direction: column;
-            min-width: 30rem;
-            border-radius: ${borders.radius};
-            box-shadow: ${shadows.light};
-            overflow: hidden;
-            opacity: 1;
-            z-index: 3000;
-            opacity: ${open ? 1 : 0};
-            transform: ${open ? 'scale(1)' : 'scale(.5)'};
-            visibility: ${open ? 'visible' : 'hidden'};
-            transition: all 0.3s;
-          `}
+          className={cx(
+            css`
+              background-color: #fff;
+              display: flex;
+              flex-direction: column;
+              min-width: 30rem;
+              border-radius: ${borders.radius};
+              box-shadow: ${shadows.light};
+              overflow: hidden;
+              opacity: 1;
+              z-index: 3000;
+              opacity: ${open ? 1 : 0};
+              transform: ${open ? 'scale(1)' : 'scale(.5)'};
+              visibility: ${open ? 'visible' : 'hidden'};
+              transition: all 0.3s;
+            `,
+            className
+          )}
+          {...rest}
         >
           {children}
         </div>
@@ -67,22 +80,31 @@ function Modal({ open, onClose, children }: Props) {
   )
 }
 
-Modal.Header = ({ children }: Pick<Props, 'children'>) => {
+Modal.Header = ({
+  children,
+  className,
+  ...rest
+}: React.HTMLProps<HTMLDivElement>) => {
   const modalContext = useContext(ModalContext)
   return (
     <div
-      className={css`
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        padding: 2rem;
-        width: 100%;
-        background: ${colors.default};
-      `}
+      className={cx(
+        css`
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          padding: 1.8rem;
+          width: 100%;
+          background: ${colors.default};
+        `,
+        className
+      )}
+      {...rest}
     >
       <span
         className={css`
           font-weight: 600;
+          font-size: 2rem;
         `}
       >
         {children}
@@ -105,11 +127,43 @@ Modal.Header = ({ children }: Pick<Props, 'children'>) => {
   )
 }
 
-Modal.Content = ({ children }: Pick<Props, 'children'>) => (
+Modal.Content = ({
+  children,
+  className,
+  ...rest
+}: React.HTMLProps<HTMLDivElement>) => {
+  return (
+    <div
+      className={cx(
+        css`
+          padding: 1.8rem;
+        `,
+        className
+      )}
+      {...rest}
+    >
+      {children}
+    </div>
+  )
+}
+
+Modal.Footer = ({
+  children,
+  className,
+  ...rest
+}: React.HTMLProps<HTMLDivElement>) => (
   <div
-    className={css`
-      padding: 2rem;
-    `}
+    className={cx(
+      css`
+        display: flex;
+        justify-content: flex-end;
+        padding: 1.8rem;
+        width: 100%;
+        border-top: ${borders.default()};
+      `,
+      className
+    )}
+    {...rest}
   >
     {children}
   </div>
