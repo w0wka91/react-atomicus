454ddb7ba7219b0f5f3a622dc28b20425fd11e3d
Waldemar Penner <Waldemar.Penner91@googlemail.com>
2018-12-05 10:58:44 +0100

    Change colors

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,8 +1,8 @@
 export const colors = {
-    default: 'hsl(211, 10%, 95%)',
-    primary: 'hsl(211, 75%, 40%)',
-    white: 'hsl(240, 100%, 99%)',
-    success: 'hsl(137, 42%, 50%)',
-    warning: 'hsl(30, 70%, 51%)',
-    danger: 'hsl(2, 75%, 54%)'
+  default: 'hsl(211, 10%, 95%)',
+  primary: 'hsl(211, 75%, 40%)',
+  white: 'hsl(240, 100%, 99%)',
+  success: '#21ba45',
+  warning: 'hsl(30, 70%, 51%)',
+  danger: '#db2828',
 }
