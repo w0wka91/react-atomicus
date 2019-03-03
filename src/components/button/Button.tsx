dc8e64e8f0bbc67b622cc7348688e838516d0a87
Waldemar Penner <Waldemar.Penner91@googlemail.com>
2019-03-03 12:32:18 +0100

    Add hierarchy to button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { css, cx } from 'emotion'
-import { darken, transparentize } from 'polished'
 import { colors } from '../../utils/colors'
-import { borders } from '../../utils/borders'
 import Icon from '../icon/Icon'
 import Spinner from '../spinner/Spinner'
+import { shadows } from '../../utils/shadows'
 
 interface Props {
   size: 'small' | 'medium' | 'large'
-  intent?: 'primary' | 'success' | 'warning' | 'danger'
+  intent: 'primary' | 'success' | 'warning' | 'danger'
+  hierarchy: 'primary' | 'secondary' | 'tertiary'
   fluid?: boolean
-  minimal?: boolean
   loading?: boolean
   children: React.ReactNode
 }
@@ -18,45 +17,112 @@ interface Props {
 function Button({
   size,
   intent,
+  hierarchy,
   fluid,
-  minimal,
   loading,
   children,
   className,
   ...rest
 }: Props & React.HTMLProps<HTMLButtonElement>) {
-  const intentColor = intent ? colors[intent] : colors.default
-  const sizes = {
+  const btnSize = {
     small: css`
       padding: 0.4rem 0.8rem;
-      min-height: 3rem;
       font-size: 1.2rem;
     `,
     medium: css`
-      padding: 0.8rem 1.2rem;
-      min-height: 4rem;
-      font-size: 1.4rem;
-    `,
-    large: css`
-      padding: 1.2rem 1.6rem;
-      min-height: 5rem;
+      padding: 0.8rem 1.6rem;
       font-size: 1.6rem;
     `,
+    large: css`
+      padding: 1.2rem 2.4rem;
+      font-size: 1.8rem;
+    `,
   }
-  const minimalStyle = css`
-    color: inherit;
-    background-color: transparent;
-    &:hover:enabled,
-    &:focus:enabled {
-      border-color: ${transparentize(0.1, intentColor)};
-      background-color: transparent;
-    }
-    &:active:enabled,
-    &:target:enabled {
-      border-color: ${intentColor};
-      background-color: transparent;
-    }
-  `
+  const btnColor = {
+    primary:
+      hierarchy === 'primary'
+        ? colors.blue500
+        : hierarchy === 'secondary'
+        ? colors.blue100
+        : 'transparent',
+    success:
+      hierarchy === 'primary'
+        ? colors.green500
+        : hierarchy === 'secondary'
+        ? colors.green100
+        : 'transparent',
+    danger:
+      hierarchy === 'primary'
+        ? colors.red500
+        : hierarchy === 'secondary'
+        ? colors.red100
+        : 'transparent',
+    warning:
+      hierarchy === 'primary'
+        ? colors.yellow500
+        : hierarchy === 'secondary'
+        ? colors.yellow100
+        : 'transparent',
+    hover: {
+      primary:
+        hierarchy === 'primary'
+          ? colors.blue600
+          : hierarchy === 'secondary'
+          ? colors.blue200
+          : 'transparent',
+      success:
+        hierarchy === 'primary'
+          ? colors.green600
+          : hierarchy === 'secondary'
+          ? colors.green200
+          : 'transparent',
+      danger:
+        hierarchy === 'primary'
+          ? colors.red600
+          : hierarchy === 'secondary'
+          ? colors.red200
+          : 'transparent',
+      warning: colors.yellow600,
+    },
+    active: {
+      primary:
+        hierarchy === 'primary'
+          ? colors.blue700
+          : hierarchy === 'secondary'
+          ? colors.blue300
+          : 'transparent',
+      success:
+        hierarchy === 'primary'
+          ? colors.green700
+          : hierarchy === 'secondary'
+          ? colors.blue300
+          : 'transparent',
+      danger:
+        hierarchy === 'primary'
+          ? colors.red700
+          : hierarchy === 'secondary'
+          ? colors.red300
+          : 'transparent',
+      warning:
+        hierarchy === 'primary'
+          ? colors.yellow700
+          : hierarchy === 'secondary'
+          ? colors.yellow300
+          : 'transparent',
+    },
+  }
+  const btnBorderColor = {
+    primary: colors.blue600,
+    success: colors.green700,
+    danger: colors.red700,
+    warning: colors.yellow700,
+  }
+  const fontColor = {
+    primary: colors.blue500,
+    success: colors.green500,
+    danger: colors.red500,
+    warning: colors.yellow500,
+  }
   return (
     <button
       className={cx(
@@ -65,34 +131,36 @@ function Button({
         align-items: center;
         justify-content: center;
         text-align: center;
-        border: ${borders.default(intentColor)};
+        border: 1px solid ${
+          hierarchy === 'primary' ? btnBorderColor[intent] : 'transparent'
+        };
         cursor: pointer;
-        border-radius: ${borders.radius};
+        border-radius: 3px;
         transition: all 0.2s;
-        font-weight: 600;
-        color: ${darken(0.6, colors.default)};
+        box-shadow: ${hierarchy === 'primary' && shadows.light};
         &:disabled {
           cursor: default;
           opacity: 0.45;
         }
-        background-color: ${intentColor};
-        &:hover:enabled,
+        background-color: ${btnColor[intent]};
+        &:hover:enabled {
+          background-color: ${btnColor.hover[intent]};
+          text-decoration: ${hierarchy === 'tertiary' && 'underline'};
+        }
         &:focus:enabled {
-          background-color: ${darken(0.035, intentColor)};
           outline: none;
         }
         &:active:enabled,
         &:target:enabled {
-          background-color: ${darken(0.07, intentColor)};
-          outline: none;
+          border-color: ${btnColor.active[intent]};
+          outline: transparent;
         }
         *:not(:last-child) {
-          margin-right: .5rem;
+          margin-right: .4rem;
         }
-        ${sizes[size]}
-        color: ${intent && 'white'};
+        ${btnSize[size]}
+        color: ${hierarchy === 'primary' ? '#FFFFFF' : fontColor[intent]};
         width: ${fluid && '100%'};
-        ${minimal && minimalStyle}
       `,
         className
       )}
@@ -104,7 +172,7 @@ function Button({
           size={1}
           sizeUnit="em"
           className={css`
-            margin-right: 1rem;
+            margin-right: 0.8rem;
           `}
         />
       )}
@@ -129,6 +197,8 @@ Button.Icon = ({ name }: ButtonIconProps) => (
 
 Button.defaultProps = {
   size: 'medium',
+  intent: 'primary',
+  hierarchy: 'primary',
 }
 
 export default Button
