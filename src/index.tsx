dceea5c46c7c855233655ba296bdf150b3fccbed
Penner, Waldemar, NMA-SF-F (Extern) <waldemar.penner.extern@bertelsmann.de>
2019-03-15 15:29:38 +0100

    Export other components

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,10 @@
+import Breadcrumb from './components/breadcrumb/Breadcrumb'
 import Button from './components/button/Button'
+import Card from './components/card/Card'
+import Checkbox from './components/checkbox/Checkbox'
+import Input from './components/input/Input'
+import Modal from './components/modal/Modal'
+import Radio from './components/radio/Radio'
+import Icon from './components/icon/Icon'
 
-export { Button }
+export { Button, Breadcrumb, Card, Checkbox, Icon, Input, Modal, Radio }
