2e1e8db47c8dc3720605f524adaea3db7af7dc37
Waldemar Penner <Waldemar.Penner91@googlemail.com>
2019-06-08 19:47:48 +0200

    fix(checkbox): fix styling issues

--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -3,6 +3,7 @@ import { css } from 'emotion'
 import { colors } from '../../utils/colors'
 import { borders } from '../../utils/borders'
 import { useId } from '../../hooks/useId'
+import { Icon } from '../..'
 
 interface Props {
   label: string
@@ -10,6 +11,9 @@ interface Props {
 
 function Checkbox({
   label,
+  value,
+  checked,
+  disabled,
   ...rest
 }: Props & React.HTMLProps<HTMLInputElement>) {
   const id = useId()
@@ -22,34 +26,14 @@ function Checkbox({
       <input
         type="checkbox"
         id={`checkbox-${id}`}
+        checked={checked}
+        disabled={disabled}
         className={css`
           position: absolute;
           opacity: 0;
 
           &:disabled + label {
             cursor: auto;
-          }
-
-          &:disabled + label::before {
-            box-shadow: none;
-            background: ${colors.grey100};
-          }
-
-          &:checked + label:after {
-            content: '';
-            position: absolute;
-            display: inline-block;
-            width: 0.4rem;
-            height: 1rem;
-            left: 0.8rem;
-            top: 1rem;
-            border: solid #ffffff;
-            border-width: 0 1px 1px 0;
-            transform: rotate(45deg);
-          }
-
-          &:checked + label:before {
-            background: ${colors.blue500};
           }
         `}
         {...rest}
@@ -58,30 +42,31 @@ function Checkbox({
         htmlFor={`checkbox-${id}`}
         className={css`
           cursor: pointer;
+          display: flex;
+          flex-direction: row;
+          align-items: center;
           color: inherit;
           font-family: inherit;
-
-          &::before {
-            content: '';
+        `}
+      >
+        <div
+          className={css`
+            display: inline-flex;
+            align-items: center;
+            justify-content: center;
             margin-right: 1.2rem;
-            vertical-align: middle;
             border-radius: ${borders.radius};
-            display: inline-block;
             width: 2rem;
             height: 2rem;
             border: 1px solid ${colors.grey300};
+            background: ${checked && colors.blue500};
+            background: ${disabled && colors.grey100};
             transition: all 0.2s linear;
-          }
-        `}
-      >
-        <span
-          className={css`
-            position: absolute;
-            top: 0.1rem;
           `}
         >
-          {label}
-        </span>
+          {checked && <Icon size="1.3rem" name="check" color="white" />}
+        </div>
+        {label}
       </label>
     </div>
   )
