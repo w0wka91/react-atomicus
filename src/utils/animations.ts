aa6ed55997fa5912ebb9ba87a4b7aab2327fc740
Waldemar <waldemar.penner91@gmail.com>
2019-08-25 14:17:12 +0200

    feat(animation): add animations

--- src/utils/animations.ts
+++ src/utils/animations.ts
@@ -55,4 +55,15 @@ const slideInUp = keyframes`
     }
 `
 
-export { bounce, zoomIn, zoomOut, slideInUp }
+const slideInDown = keyframes`
+  from {
+    transform: translate3d(0, -100%, 0);
+    visibility: visible;
+  }
+
+  to {
+    transform: translate3d(0, 0, 0);
+  }
+`
+
+export { bounce, zoomIn, zoomOut, slideInUp, slideInDown }
